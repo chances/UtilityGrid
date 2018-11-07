662e75ea0fd0f6895c0de302754e2bae66bd4b6b
Chance Snow <git@chancesnow.me>
2018-11-06 16:42:28 -0800

    Hide OS Ui overlays so app is fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(UtilityGridGame());
 
 class UtilityGridGame extends StatelessWidget {
+  UtilityGridGame() {
+    // Hide OS UI overlays so the game is fullscreen
+    SystemChrome.setEnabledSystemUIOverlays([]);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
