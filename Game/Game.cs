5364ae35bcc1d90a4b2ab798bc2aceaeffd55675
Chance Snow <git@chancesnow.me>
2018-11-18 02:19:44 -0800

    Refactor, move OpenGL graphics device creation to static class helper

--- Game/Game.cs
+++ Game/Game.cs
@@ -4,7 +4,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Engine;
 using Engine.Components;
-using Engine.ECS;
 using Veldrid;
 using Veldrid.OpenGL;
 using Veldrid.Sdl2;
@@ -32,7 +31,7 @@ namespace Game
 
         protected override GraphicsDevice CreateGraphicsDevice()
         {
-            var windowCreateInfo = new WindowCreateInfo()
+            var windowCreateInfo = new WindowCreateInfo
             {
                 X = 100,
                 Y = 100,
@@ -49,53 +48,7 @@ namespace Game
 
             return RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
                 ? VeldridStartup.CreateGraphicsDevice(_window)
-                : CreateOpenGLGraphicsDevice(_window, new GraphicsDeviceOptions(false));
-        }
-
-        private static unsafe GraphicsDevice CreateOpenGLGraphicsDevice(Sdl2Window window, GraphicsDeviceOptions options)
-        {
-            Sdl2Native.SDL_ClearError();
-            var sdlHandle = window.SdlWindowHandle;
-            SDL_SysWMinfo sdlSysWmInfo;
-            Sdl2Native.SDL_GetVersion(&sdlSysWmInfo.version);
-            Sdl2Native.SDL_GetWMWindowInfo((SDL_Window) sdlHandle, &sdlSysWmInfo);
-
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.ContextFlags, options.Debug ? 3 : 2);
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.ContextProfileMask, 1);
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.ContextMajorVersion, 4);
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.ContextMinorVersion, 1);
-
-            options.SwapchainDepthFormat = PixelFormat.D32_Float_S8_UInt;
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.DepthSize, 32);
-            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.StencilSize, 8);
-
-            var contextHandle = Sdl2Native.SDL_GL_CreateContext((SDL_Window) sdlHandle);
-            var error = Sdl2Native.SDL_GetError();
-            if ((IntPtr) error != IntPtr.Zero)
-            {
-                string str = GetString(error);
-                if (!string.IsNullOrEmpty(str))
-                    throw new VeldridException(string.Format("Unable to create OpenGL Context: \"{0}\". This may indicate that the system does not support the requested OpenGL profile, version, or Swapchain format.", (object) str));
-            }
-            int num1;
-            Sdl2Native.SDL_GL_GetAttribute(SDL_GLAttribute.DepthSize, &num1);
-            int num2;
-            Sdl2Native.SDL_GL_GetAttribute(SDL_GLAttribute.StencilSize, &num2);
-            Sdl2Native.SDL_GL_SetSwapInterval(options.SyncToVerticalBlank ? 1 : 0);
-            var getProcAddress = new Func<string, IntPtr>(Sdl2Native.SDL_GL_GetProcAddress);
-            var makeCurrent = (Action<IntPtr>) (context => Sdl2Native.SDL_GL_MakeCurrent((SDL_Window) sdlHandle, context));
-            var deleteContext = new Action<IntPtr>(Sdl2Native.SDL_GL_DeleteContext);
-            var swapBuffers = (Action) (() => Sdl2Native.SDL_GL_SwapWindow((SDL_Window) sdlHandle));
-            var platformInfo = new OpenGLPlatformInfo(contextHandle, getProcAddress, makeCurrent, (Func<IntPtr>) (Sdl2Native.SDL_GL_GetCurrentContext), (Action) (() => Sdl2Native.SDL_GL_MakeCurrent(new SDL_Window(IntPtr.Zero), IntPtr.Zero)), deleteContext, swapBuffers, (Action<bool>) (sync => Sdl2Native.SDL_GL_SetSwapInterval(sync ? 1 : 0)));
-            return GraphicsDevice.CreateOpenGL(options, platformInfo, (uint) window.Width, (uint) window.Height);
-        }
-
-        private static unsafe string GetString(byte* stringStart)
-        {
-            var byteCount = 0;
-            while (stringStart[byteCount] != (byte) 0)
-                ++byteCount;
-            return Encoding.UTF8.GetString(stringStart, byteCount);
+                : GraphicsDeviceUtils.CreateOpenGLGraphicsDevice(_window, new GraphicsDeviceOptions(false));
         }
 
         protected override void Initialize()
